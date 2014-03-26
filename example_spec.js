002d9ac18055db519c65f3a36c3a448f5b4baa08
Jeffrey Biles <jeffreybiles@Jeffreys-MacBook-Pro-2.local>
2014-03-26 11:55:20 -0500

    testing spike

--- example_spec.js
+++ example_spec.js
@@ -3,3 +3,45 @@ describe("Testing", function(){
     expect(1 + 1).to.equal(2)
   })
 })
+
+Ember.Test.adapter = Ember.Test.MochaAdapter.create();
+App.setupForTesting();
+App.injectTestHelpers();
+
+
+rungify = function(original) {
+  return function(fn) {
+    if (fn.length) {
+      return original(function(done) {
+        return Ember.run((function(_this) {
+          return function() {
+            return fn.call(_this, done);
+          };
+        })(this));
+      });
+    } else {
+      return original(function() {
+        return Ember.run((function(_this) {
+          return function() {
+            return fn.call(_this);
+          };
+        })(this));
+      });
+    }
+  };
+};
+
+this.beforeEach = rungify(this.beforeEach);
+
+this.afterEach = rungify(this.afterEach);
+
+describe("Index Page", function(){
+  beforeEach(function(){
+    visit('/posts')
+  })
+
+  it("has two posts", function(){
+    console.log($('table'))
+    expect($('#posts .post-link').length).to.equal(2);
+  })
+})
